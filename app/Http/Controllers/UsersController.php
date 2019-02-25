f2a0a6b2cecf492caa08383155cf5b873f250b34
conrado_carrillo <anton_con@hotmail.com>
2019-02-24 18:14:44 -0600

    Se corrigió la query para traer los usuarios recepcionistas por franquicias

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,7 +32,7 @@ class UsersController extends Controller
 
 			$users = User::whereIn('role_id',$roles_id)->get();
 		} else {
-			$users = User::where('branch_id', auth()->user()->branches->pluck('id'))->get();
+			$users = User::whereIn('branch_id', auth()->user()->branches->pluck('id'))->get();
 		}
 
 		$roles = Role::all()->pluck('name','id')->prepend('Todos los roles', 0)->except(['id', '4']);
